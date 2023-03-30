171083a9d47ad63c2d380185e1290f5c4fe1f3b0
Larry Crochet <crochet_larry@bah.com>
2023-03-30 06:22:23 -0400

    claims_user

--- modules/claims_api/app/controllers/concerns/claims_api/user_identifier.rb
+++ modules/claims_api/app/controllers/concerns/claims_api/user_identifier.rb
@@ -1,24 +1,19 @@
 module ClaimsApi
-  class ClaimsUser
+  class UserIdentifier
     def initialize(id)
       @id = id
-      @uuid = id
       @loa = {:current => 3, :highest => 3}
-      @identifier = UserIdentifier.new(id)
     end
 
     def set_icn(icn)
       @icn = icn
-      @identifier.set_icn(icn)
     end
 
     attr_reader :icn
-    attr_reader :uuid
 
     def first_name_last_name(first_name, last_name)
       @first_name = first_name
       @last_name = last_name
-      @identifier.first_name_last_name(first_name, last_name)
     end
 
     attr_reader :first_name
