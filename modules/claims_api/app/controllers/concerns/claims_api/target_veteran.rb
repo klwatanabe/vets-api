63308ee68373faa724caece446a92e635033af1f
Larry Crochet <crochet_larry@bah.com>
2023-04-07 12:24:00 -0400

    linting

--- modules/claims_api/app/controllers/concerns/claims_api/target_veteran.rb
+++ modules/claims_api/app/controllers/concerns/claims_api/target_veteran.rb
@@ -8,7 +8,7 @@ module ClaimsApi
 
     included do
       def target_veteran
-        @target_veteran ||= if @validated_token_payload && @current_user.icn != nil
+        @target_veteran ||= if @validated_token_payload && !@current_user.icn.nil?
                               build_target_veteran(veteran_id: @current_user.icn, loa: { current: 3, highest: 3 })
                             elsif user_is_representative?
                               build_target_veteran(veteran_id: params[:veteranId], loa: @current_user.loa)
@@ -28,16 +28,20 @@ module ClaimsApi
 
         unless found_record
           raise ::Common::Exceptions::ResourceNotFound.new(detail:
-                                                             "Unable to locate Veteran's ID/ICN in Master Person Index (MPI). " \
-                                                             'Please submit an issue at ask.va.gov or call 1-800-MyVA411 (800-698-2411) for assistance.')
+                                                             "Unable to locate Veteran's ID/ICN " \
+                                                             'in Master Person Index (MPI). ' \
+                                                             'Please submit an issue at ask.va.gov ' \
+                                                             'or call 1-800-MyVA411 (800-698-2411) for assistance.')
         end
 
         mpi_profile = target_veteran&.mpi&.mvi_response&.profile || {}
 
         if mpi_profile[:participant_id].blank?
           raise ::Common::Exceptions::UnprocessableEntity.new(detail:
-                                                                "Unable to locate Veteran's Participant ID in Master Person Index (MPI). " \
-                                                                'Please submit an issue at ask.va.gov or call 1-800-MyVA411 (800-698-2411) for assistance.')
+                                                                "Unable to locate Veteran's Participant ID " \
+                                                                "in Master Person Index (MPI). " \
+                                                                'Please submit an issue at ask.va.gov ' \
+                                                                'or call 1-800-MyVA411 (800-698-2411) for assistance.')
         end
 
         target_veteran[:first_name] = mpi_profile[:given_names]&.first
