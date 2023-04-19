95490903e97700c7d002363cb65034db8c208bf3
Carly Jones <carly.jones@coforma.io>
2023-04-19 18:25:58 -0400

    Linting

--- lib/sidekiq/form526_historical_data_exporter/exporter.rb
+++ lib/sidekiq/form526_historical_data_exporter/exporter.rb
@@ -3,7 +3,6 @@
 module Sidekiq
   module Form526HistoricalDataExporter
     class Exporter
-
       def initialize(batch_size, data = [])
         @batch_size = batch_size
         @data = data
@@ -12,14 +11,15 @@ module Sidekiq
       # add a basic method for testing
       def print_to_stdout!
         all = []
-        batches = Form526Submission.select(:id, :created_at, :encrypted_kms_key, :form_json_ciphertext, :submitted_claim_id).find_in_batches(batch_size: @batch_size)
+        batches = Form526Submission.select(:id, :created_at, :encrypted_kms_key, :form_json_ciphertext,
+                                           :submitted_claim_id).find_in_batches(batch_size: @batch_size)
         batches.each do |batch|
           batch.each do |submission|
             all << [submission.id, submission.submitted_claim_id, submission.created_at, submission.form_json]
           end
         end
-        puts all.size
-        puts all.first
+        # puts all.size
+        # puts all.first
       end
 
       def process!
