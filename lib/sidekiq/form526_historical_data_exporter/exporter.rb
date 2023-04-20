0137176b4d811d9b10d1335883b278b0e9d96ced
Carly Jones <carly.jones@coforma.io>
2023-04-20 13:17:47 -0400

    Minor updates

--- lib/sidekiq/form526_historical_data_exporter/exporter.rb
+++ lib/sidekiq/form526_historical_data_exporter/exporter.rb
@@ -3,8 +3,8 @@
 module Sidekiq
   module Form526HistoricalDataExporter
     class Exporter
-      def initialize(batch_size, data = [])
-        @batch_size = batch_size
+      def initialize(b_size, data = [])
+        @bs = b_size.to_i
         @data = data
       end
 
@@ -12,18 +12,17 @@ module Sidekiq
       def print_to_stdout!
         all = []
         batches = Form526Submission.select(:id, :created_at, :encrypted_kms_key, :form_json_ciphertext,
-                                           :submitted_claim_id).find_in_batches(batch_size: @batch_size)
+                                           :submitted_claim_id).find_in_batches(batch_size: @bs)
         batches.each do |batch|
           batch.each do |submission|
             all << [submission.id, submission.submitted_claim_id, submission.created_at, submission.form_json]
           end
         end
-        # puts all.size
-        # puts all.first
+        puts all.first
       end
 
       def process!
-        # # do useful data stuff here, for now just pass
+        # find records in batches, save to enumerator, iterate over enumerator to decrypt each batch
       end
     end
   end
