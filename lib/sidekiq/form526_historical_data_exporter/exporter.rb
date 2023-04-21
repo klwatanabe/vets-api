74a8c8c0a358f5652f483209225e9e60261406f3
Kyle Soskin <kyle.soskin@adhocteam.us>
2023-04-21 07:42:41 -0400

    Adding stuff from meeting

--- lib/sidekiq/form526_historical_data_exporter/exporter.rb
+++ lib/sidekiq/form526_historical_data_exporter/exporter.rb
@@ -3,27 +3,41 @@
 module Sidekiq
   module Form526HistoricalDataExporter
     class Exporter
-      def initialize(b_size, data = [])
+      def initialize(b_size, start_id, end_id, data = [])
         @bs = b_size.to_i
         @data = data
+        @file_name = "/tmp/#{start_id}_#{end_id}.csv"
       end
 
+      # have it take start and end Form526Submission IDs
+      # pass them into the batch
+      # write to file
+      def write_to_file(content)
+        File.write(@file_name, content)
+      end
+
+      # put file in s3
+      def upload_to_s3!
+        # fill in here
+        #
+      end
+      
+      
       # add a basic method for testing
-      def print_to_stdout!
+      def process!
         all = []
         batches = Form526Submission.select(:id, :created_at, :encrypted_kms_key, :form_json_ciphertext,
-                                           :submitted_claim_id).find_in_batches(batch_size: @bs)
+                                           :submitted_claim_id).find_in_batches(batch_size: @bs, start: @start_id, finish: @end_id)
         batches.each do |batch|
           batch.each do |submission|
             all << [submission.id, submission.submitted_claim_id, submission.created_at, submission.form_json]
           end
         end
-        puts all.first
+        # Fix to csv to get into something useable. Maybe yaml instead? 
+        # OR JSON!
+        write_to_file(all.to_json)
       end
 
-      def process!
-        # find records in batches, save to enumerator, iterate over enumerator to decrypt each batch
-      end
     end
   end
 end
--- lib/sidekiq/form526_historical_data_exporter/submit.rb
+++ lib/sidekiq/form526_historical_data_exporter/submit.rb
@@ -4,11 +4,28 @@ require 'sidekiq/form526_historical_data_exporter/exporter'
 
 module Sidekiq
   module Form526HistoricalDataExporter
+
+    class Form526BackgroundDataJobQueuer
+      def initialize(batch_size)
+        queue_chunks(chunk_submissions(batch_size))
+      end
+
+      def chunk_submissions(batch_size)
+        Form526Submission.where('created_at >= ?', 3.years.ago).pluck(:id).each_slice(batch_size)
+      end
+
+      def queue_chunks(chunks)
+        chunks.each do |chunk|
+          Form526BackgroundDataJob.perform(batch_size, chunk.first, chunk.last)
+        end
+      end
+    end
+
     class Form526BackgroundDataJob
       include Sidekiq::Worker
 
-      def perform(b_size)
-        Exporter.new(b_size).print_to_stdout!
+      def perform(batch_size, start_id, end_id)
+        Exporter.new(batch_size, start_id, end_id).process!
       end
     end
   end
