65c16a6614da0ab409e0dd5cec117029e0dd761e
Carly Jones <carly.jones@coforma.io>
2023-04-21 15:23:26 -0400

    Minor updates, debugging, linting

--- lib/sidekiq/form526_historical_data_exporter/exporter.rb
+++ lib/sidekiq/form526_historical_data_exporter/exporter.rb
@@ -3,8 +3,10 @@
 module Sidekiq
   module Form526HistoricalDataExporter
     class Exporter
-      def initialize(b_size, start_id, end_id, data = [])
-        @bs = b_size.to_i
+      def initialize(batch_size, start_id, end_id, data = [])
+        @batch_size = batch_size.to_i
+        @start_id = start_id
+        @end_id = end_id
         @data = data
         @file_name = "/tmp/#{start_id}_#{end_id}.csv"
       end
@@ -19,25 +21,22 @@ module Sidekiq
       # put file in s3
       def upload_to_s3!
         # fill in here
-        #
       end
-      
-      
-      # add a basic method for testing
+
       def process!
         all = []
         batches = Form526Submission.select(:id, :created_at, :encrypted_kms_key, :form_json_ciphertext,
-                                           :submitted_claim_id).find_in_batches(batch_size: @bs, start: @start_id, finish: @end_id)
+                                           :submitted_claim_id).find_in_batches(batch_size: @batch_size, start: @start_id, finish: @end_id)
         batches.each do |batch|
           batch.each do |submission|
             all << [submission.id, submission.submitted_claim_id, submission.created_at, submission.form_json]
           end
         end
-        # Fix to csv to get into something useable. Maybe yaml instead? 
+        # Fix to csv to get into something useable. Maybe yaml instead?
         # OR JSON!
         write_to_file(all.to_json)
+        # puts all.to_json
       end
-
     end
   end
 end
--- lib/sidekiq/form526_historical_data_exporter/submit.rb
+++ lib/sidekiq/form526_historical_data_exporter/submit.rb
@@ -4,19 +4,19 @@ require 'sidekiq/form526_historical_data_exporter/exporter'
 
 module Sidekiq
   module Form526HistoricalDataExporter
-
     class Form526BackgroundDataJobQueuer
-      def initialize(batch_size)
-        queue_chunks(chunk_submissions(batch_size))
+      def initialize(chunk_size, batch_size) # add path to storage as argument? if so, add as attribute
+        @batch_size = batch_size
+        queue_chunks(chunk_submissions(chunk_size))
       end
 
-      def chunk_submissions(batch_size)
-        Form526Submission.where('created_at >= ?', 3.years.ago).pluck(:id).each_slice(batch_size)
+      def chunk_submissions(chunk_size)
+        Form526Submission.where('created_at >= ?', 3.years.ago).pluck(:id).each_slice(chunk_size)
       end
 
       def queue_chunks(chunks)
         chunks.each do |chunk|
-          Form526BackgroundDataJob.perform(batch_size, chunk.first, chunk.last)
+          Form526BackgroundDataJob.perform(@batch_size, chunk.first, chunk.last) # add path to storage here
         end
       end
     end
@@ -24,7 +24,7 @@ module Sidekiq
     class Form526BackgroundDataJob
       include Sidekiq::Worker
 
-      def perform(batch_size, start_id, end_id)
+      def self.perform(batch_size, start_id, end_id)
         Exporter.new(batch_size, start_id, end_id).process!
       end
     end
