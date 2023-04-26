b9a33d107e7a4d6f875752cd736514d03a5db8d1
Jennica Stiehl <25069483+stiehlrod@users.noreply.github.com>
2023-04-26 14:19:00 -0600

    Adds to request spec file

--- modules/claims_api/spec/requests/v2/veterans/disability_compensation_request_spec.rb
+++ modules/claims_api/spec/requests/v2/veterans/disability_compensation_request_spec.rb
@@ -11,7 +11,7 @@ RSpec.describe 'Disability Claims', type: :request do
       'X-VA-Birth-Date': '1986-05-06T00:00:00+00:00',
       'X-VA-Gender': 'M' }
   end
-  let(:scopes) { %w[system/claim.write] }
+  let(:scopes) { %w[claim.write claim.read] }
   let(:schema) { Rails.root.join('modules', 'claims_api', 'config', 'schemas', 'v2', '526.json').read }
 
   before do
@@ -26,6 +26,237 @@ RSpec.describe 'Disability Claims', type: :request do
 
   describe '#526' do
     context 'submit' do
+      let(:claim_date) { (Time.zone.today - 1.day).to_s }
+      let(:data) do
+        temp = Rails.root.join('modules', 'claims_api', 'spec', 'fixtures', 'v2', 'veterans', 'disability_compensation',
+                               'form_526_json_api.json').read
+        temp = JSON.parse(temp)
+        temp['data']['attributes']['claimDate'] = claim_date
+
+        temp.to_json
+      end
+      let(:veteran_id) { '1012667145V762142' }
+      let(:path) { "/services/claims/v2/veterans/#{veteran_id}/526" }
+      let(:schema) { Rails.root.join('modules', 'claims_api', 'config', 'schemas', 'v2', '526.json').read }
+      let(:parsed_codes) do
+        {
+          birls_id: '111985523',
+          participant_id: '32397028'
+        }
+      end
+      let(:add_response) { build(:add_person_response, parsed_codes:) }
+
+      before do
+        Timecop.freeze(Time.parse('2022-05-01 12:00:00 UTC'))
+      end
+
+      after do
+        Timecop.return
+      end
+
+      # real world example happened in API-15575
+      describe "'claim_date' difference between Lighthouse (UTC) and EVSS (Central Time)" do
+        context 'when UTC is currently a day ahead of the US Central Time Zone' do
+          before do
+            Timecop.freeze(Time.parse('2022-05-01 04:46:31 UTC'))
+          end
+
+          after do
+            Timecop.return
+          end
+
+          context "and 'claim_date' is same as the Central Time Zone day" do
+            let(:claim_date) { (Time.zone.today - 1.day).to_s }
+
+            it 'responds with a 200' do
+              with_okta_user(scopes) do |auth_header|
+                VCR.use_cassette('evss/claims/claims') do
+                  VCR.use_cassette('evss/reference_data/countries') do
+                    post path, params: data, headers: headers.merge(auth_header)
+                    expect(response).to have_http_status(:ok)
+                  end
+                end
+              end
+            end
+          end
+
+          context "and 'claim_date' is earlier than the Central Time Zone day" do
+            let(:claim_date) { (Time.zone.today - 7.days).to_s }
+
+            it 'responds with a 200' do
+              with_okta_user(scopes) do |auth_header|
+                VCR.use_cassette('evss/claims/claims') do
+                  VCR.use_cassette('evss/reference_data/countries') do
+                    post path, params: data, headers: headers.merge(auth_header)
+                    expect(response).to have_http_status(:ok)
+                  end
+                end
+              end
+            end
+          end
+
+          context "and 'claim_date' is later than both the Central Time Zone day and UTC day" do
+            let(:claim_date) { (Time.zone.today + 7.days).to_s }
+
+            it 'responds with a bad request' do
+              with_okta_user(scopes) do |auth_header|
+                VCR.use_cassette('evss/claims/claims') do
+                  post path, params: data, headers: headers.merge(auth_header)
+                  expect(response).to have_http_status(:bad_request)
+                end
+              end
+            end
+          end
+        end
+
+        context 'when UTC is same day as the US Central Time Zone day' do
+          before do
+            Timecop.freeze(Time.parse('2022-05-01 12:00:00 UTC'))
+          end
+
+          after do
+            Timecop.return
+          end
+
+          context "and 'claim_date' is the current day" do
+            let(:claim_date) { Time.zone.today.to_s }
+
+            it 'responds with a 200' do
+              with_okta_user(scopes) do |auth_header|
+                VCR.use_cassette('evss/claims/claims') do
+                  VCR.use_cassette('evss/reference_data/countries') do
+                    post path, params: data, headers: headers.merge(auth_header)
+                    expect(response).to have_http_status(:ok)
+                  end
+                end
+              end
+            end
+          end
+
+          context "and 'claim_date' is in the past" do
+            let(:claim_date) { (Time.zone.today - 1.day).to_s }
+
+            it 'responds with a 200' do
+              with_okta_user(scopes) do |auth_header|
+                VCR.use_cassette('evss/claims/claims') do
+                  VCR.use_cassette('evss/reference_data/countries') do
+                    post path, params: data, headers: headers.merge(auth_header)
+                    expect(response).to have_http_status(:ok)
+                  end
+                end
+              end
+            end
+          end
+
+          context "and 'claim_date' is in the future" do
+            let(:claim_date) { (Time.zone.today + 1.day).to_s }
+
+            it 'responds with bad request' do
+              with_okta_user(scopes) do |auth_header|
+                VCR.use_cassette('evss/claims/claims') do
+                  post path, params: data, headers: headers.merge(auth_header)
+                  expect(response).to have_http_status(:bad_request)
+                end
+              end
+            end
+          end
+
+          context "and 'claim_date' has timezone (iso w/Z)" do
+            let(:claim_date) { 1.day.ago.iso8601 }
+
+            it 'responds with a 200' do
+              with_okta_user(scopes) do |auth_header|
+                VCR.use_cassette('evss/claims/claims') do
+                  VCR.use_cassette('evss/reference_data/countries') do
+                    post path, params: data, headers: headers.merge(auth_header)
+                    expect(response).to have_http_status(:ok)
+                  end
+                end
+              end
+            end
+          end
+
+          context "and 'claim_date' has timezone (iso wo/Z)" do
+            let(:claim_date) { 1.day.ago.iso8601.sub('Z', '-00:00') }
+
+            it 'responds with a 200' do
+              with_okta_user(scopes) do |auth_header|
+                VCR.use_cassette('evss/claims/claims') do
+                  VCR.use_cassette('evss/reference_data/countries') do
+                    post path, params: data, headers: headers.merge(auth_header)
+                    expect(response).to have_http_status(:ok)
+                  end
+                end
+              end
+            end
+          end
+
+          context "and 'claim_date' has timezone (iso w/out zone)" do
+            let(:claim_date) { 1.day.ago.iso8601.sub('Z', '') }
+
+            it 'responds with a bad request' do
+              with_okta_user(scopes) do |auth_header|
+                VCR.use_cassette('evss/claims/claims') do
+                  post path, params: data, headers: headers.merge(auth_header)
+                  expect(response).to have_http_status(:unprocessable_entity)
+                end
+              end
+            end
+          end
+
+          context "and 'claim_date' has timezone (TZ String)" do
+            let(:claim_date) { 1.day.ago.to_s }
+
+            it 'responds with a 422' do
+              with_okta_user(scopes) do |auth_header|
+                VCR.use_cassette('evss/claims/claims') do
+                  post path, params: data, headers: headers.merge(auth_header)
+                  expect(response).to have_http_status(:unprocessable_entity)
+                end
+              end
+            end
+          end
+
+          context "and 'claim_date' has timezone (w/out T)" do
+            let(:claim_date) { 1.day.ago.iso8601.sub('T', ' ') }
+
+            it 'responds with a 422' do
+              with_okta_user(scopes) do |auth_header|
+                VCR.use_cassette('evss/claims/claims') do
+                  post path, params: data, headers: headers.merge(auth_header)
+                  expect(response).to have_http_status(:unprocessable_entity)
+                end
+              end
+            end
+          end
+
+          context "and 'claim_date' improperly formatted (hello world)" do
+            let(:claim_date) { 'hello world' }
+
+            it 'responds with bad request' do
+              with_okta_user(scopes) do |auth_header|
+                VCR.use_cassette('evss/claims/claims') do
+                  post path, params: data, headers: headers.merge(auth_header)
+                  expect(response).to have_http_status(:unprocessable_entity)
+                end
+              end
+            end
+          end
+
+          context "and 'claim_date' improperly formatted (empty string)" do
+            let(:claim_date) { '' }
+
+            it 'responds with bad request' do
+              with_okta_user(scopes) do |auth_header|
+                VCR.use_cassette('evss/claims/claims') do
+                  post path, params: data, headers: headers.merge(auth_header)
+                  expect(response).to have_http_status(:unprocessable_entity)
+                end
+              end
+            end
+          end
+        end
+      end
     end
 
     context 'validate' do
