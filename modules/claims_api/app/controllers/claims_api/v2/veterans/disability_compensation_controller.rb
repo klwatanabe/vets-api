cf210547f563a5321b7cbe274976b5493562a6f5
Jennica Stiehl <25069483+stiehlrod@users.noreply.github.com>
2023-04-26 14:18:20 -0600

    Adds validation to controller.

--- modules/claims_api/app/controllers/claims_api/v2/veterans/disability_compensation_controller.rb
+++ modules/claims_api/app/controllers/claims_api/v2/veterans/disability_compensation_controller.rb
@@ -2,14 +2,19 @@
 
 require 'common/exceptions'
 require 'jsonapi/parser'
+require 'claims_api/v2/concerns/disabilitity_compensation/validation'
 
 module ClaimsApi
   module V2
     module Veterans
       class DisabilityCompensationController < ClaimsApi::V2::ApplicationController
+        include ClaimsApi::V2::Concerns::DisabilityCompensation::Validation
+
         FORM_NUMBER = '526'
 
-        def submit; end
+        def submit
+          render json: { status: 200}
+        end
 
         def validate; end
 
