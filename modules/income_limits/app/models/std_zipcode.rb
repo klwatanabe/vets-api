87328eb437889fd4eb6b4872350e8ec12d7234ac
jtmst <josh.troy.mills@gmail.com>
2023-05-18 11:31:27 -0400

    model name update

--- modules/income_limits/app/models/std_zipcode.rb
+++ modules/income_limits/app/models/std_zipcode.rb
@@ -1,13 +1,11 @@
 # frozen_string_literal: true
 
-module IncomeLimits
-  class StdZipcode < ApplicationRecord
-    self.table_name = 'std_zipcodes'
-    validates :id, presence: true, uniqueness: true
-    validates :zip_code, presence: true
-    validates :state_id, presence: true
-    validates :county_number, presence: true
-    validates :version, presence: true
-    validates :created, presence: true
-  end
+class StdZipcode < ApplicationRecord
+  self.table_name = 'std_zipcodes'
+  validates :id, presence: true, uniqueness: true
+  validates :zip_code, presence: true
+  validates :state_id, presence: true
+  validates :county_number, presence: true
+  validates :version, presence: true
+  validates :created, presence: true
 end
