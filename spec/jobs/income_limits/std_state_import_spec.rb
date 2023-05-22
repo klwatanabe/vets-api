650ffc05cc9e720cabb1eb7a5d4fc674709db4fb
jtmst <josh.troy.mills@gmail.com>
2023-05-22 10:31:07 -0400

    test vcr

--- spec/jobs/income_limits/std_state_import_spec.rb
+++ spec/jobs/income_limits/std_state_import_spec.rb
@@ -5,6 +5,7 @@ require 'csv'
 
 RSpec.describe IncomeLimits::StdStateImport, type: :worker do
   describe '#perform' do
+    let(:csv_url) { 'https://sitewide-public-websites-income-limits-data.s3-us-gov-west-1.amazonaws.com/std_state.csv' }
     let(:csv_data) do
       <<-CSV
         ID,NAME,POSTALNAME,FIPSCODE,COUNTRY_ID,VERSION,CREATED,UPDATED,CREATEDBY,UPDATEDBY
@@ -13,38 +14,44 @@ RSpec.describe IncomeLimits::StdStateImport, type: :worker do
     end
 
     before do
-      allow(URI).to receive(:open).and_return(StringIO.new(csv_data))
+      allow(URI).to receive(:open).with(csv_url).and_return(StringIO.new(csv_data))
     end
 
     context 'when a matching record already exists' do
-      let!(:existing_state) { create(:std_state, id: 1) }
+      VCR.use_cassette('std_state_import/matching_record_exists') do
+        let!(:existing_state) { create(:std_state, id: 1) }
 
-      it 'does not create a new record' do
-        expect do
-          described_class.new.perform
-        end.not_to change(StdState, :count)
+        it 'does not create a new record' do
+          expect do
+            described_class.new.perform
+          end.not_to change(StdState, :count)
+        end
       end
     end
 
     context 'when a matching record does not exist' do
       it 'creates a new record' do
-        expect do
-          described_class.new.perform
-        end.to change(StdState, :count).by(1)
+        VCR.use_cassette(std_state_import/no_matching_record) do
+          expect do
+            described_class.new.perform
+          end.to change(StdState, :count).by(1)
+        end
       end
 
       it 'sets the attributes correctly' do
-        described_class.new.perform
-        state = StdState.last
-        expect(state.name).to eq('State A')
-        expect(state.postal_name).to eq('Postal A')
-        expect(state.fips_code).to eq(123)
-        expect(state.country_id).to eq(2)
-        expect(state.version).to eq(1)
-        expect(state.created).to eq(Date.new(2023, 1, 1))
-        expect(state.updated).to eq(Date.new(2023, 1, 2))
-        expect(state.created_by).to eq('John')
-        expect(state.updated_by).to eq('Doe')
+        VCR.use_cassette(std_state_import/no_matching_record) do
+          described_class.new.perform
+          state = StdState.last
+          expect(state.name).to eq('State A')
+          expect(state.postal_name).to eq('Postal A')
+          expect(state.fips_code).to eq(123)
+          expect(state.country_id).to eq(2)
+          expect(state.version).to eq(1)
+          expect(state.created).to eq(Date.new(2023, 1, 1))
+          expect(state.updated).to eq(Date.new(2023, 1, 2))
+          expect(state.created_by).to eq('John')
+          expect(state.updated_by).to eq('Doe')
+        end
       end
     end
   end
