9cc4ce2d02c149830a24317138254c7ee39742ae
jtmst <josh.troy.mills@gmail.com>
2023-05-22 15:17:20 -0400

    test data

--- spec/jobs/income_limits/std_state_import_spec.rb
+++ spec/jobs/income_limits/std_state_import_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe IncomeLimits::StdStateImport, type: :worker do
     let(:csv_data) do
       <<-CSV
         ID,NAME,POSTALNAME,FIPSCODE,COUNTRY_ID,VERSION,CREATED,UPDATED,CREATEDBY,UPDATEDBY
-        1,State A,Postal A,123,2,1,7/21/2005 12:13:36.000000 PM,7/22/2005 12:13:36.000000 PM,John,Sam
+        1,Maine,Sample County,123,2,1,7/21/2005 12:13:36.000000 PM,7/22/2005 12:13:36.000000 PM,John,Sam
       CSV
     end
 
@@ -17,6 +17,12 @@ RSpec.describe IncomeLimits::StdStateImport, type: :worker do
       allow_any_instance_of(
         IncomeLimits::StdStateImport
       ).to receive(:fetch_csv_data).and_return(response)
+    end
+
+    it 'populates states' do
+      IncomeLimits::StdStateImport.new.perform
+      expect(StdState.find('Maine')).not_to be_nil
+      expect(StdState.find('123')).not_to be_nil
     end
 
     it 'creates a new StdState record' do
@@ -28,7 +34,7 @@ RSpec.describe IncomeLimits::StdStateImport, type: :worker do
     it 'sets the attributes correctly' do
       described_class.new.perform
       state = StdState.last
-      expect(state.name).to eq('State A')
+      expect(state.name).to eq('Maine')
       expect(state.postal_name).to eq('Postal A')
       expect(state.fips_code).to eq(123)
       expect(state.country_id).to eq(2)
