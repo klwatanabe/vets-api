039aeb04ff940ca9206cdc437c0911e43461cc74
Melissa Miller <mmiller@pluribusdigital.com>
2023-06-09 15:13:28 -0400

    Initial reactivation email job

---
+++ app/workers/login_adoption_email_job.rb
@@ -0,0 +1,59 @@
+# frozen_string_literal: true
+
+class LoginAdoptionEmailJob
+  include Sidekiq::Worker
+
+  sidekiq_options retry: false
+
+  REACTIVATION_TEMPLATE = Settings.vanotify.services.va_gov.template_id.login_reactivation_email
+
+  def perform(user)
+    @user = user
+
+    return unless signed_in_with_legacy_credential?
+    return unless Flipper.enabled?(:reactivation_experiment, user)
+
+    send_email if user_qualifies_for_reactivation?
+  end
+
+  def send_email
+    email = @user.email
+
+    return if email.blank?
+
+    VANotify::EmailJob.perform_async(
+      email,
+      REACTIVATION_TEMPLATE,
+      {
+        # personalization stuff goes here
+      }
+    )
+  end
+
+  private
+
+  def credential_type
+    @credential_type ||= @user.identity.sign_in[:service_name]
+  end
+
+  def logged_in_with_dsl?
+    credential_type == SAML::User::DSLOGON_CSID
+  end
+
+  def logged_in_with_mhv?
+    credential_type == SAML::User::MHV_ORIGINAL_CSID
+  end
+
+  def signed_in_with_legacy_credential?
+    logged_in_with_dsl? || logged_in_with_mhv?
+  end
+
+  def verified_credential_at?
+    user_avc = UserAcceptableVerifiedCredential.find_by(user_account: @user.user_account)
+    user_avc&.acceptable_verified_credential_at || user_avc&.idme_verified_credential_at
+  end
+
+  def user_qualifies_for_reactivation?
+    signed_in_with_legacy_credential? && verified_credential_at?
+  end
+end
---
+++ spec/jobs/login_adoption_email_job_spec.rb
@@ -0,0 +1,252 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe LoginAdoptionEmailJob do
+  let(:job) { described_class.new }
+  let(:reactivation_template) { Settings.vanotify.services.va_gov.template_id.login_reactivation_email }
+  let(:user_account) { user_verification.user_account }
+
+  describe '#perform' do
+    context 'When Flipper reactivation_experiment is enabled' do
+      context 'User is dslogon authenticated' do
+        let(:user) { create(:user, :dslogon) }
+        let(:user_verification) { create(:dslogon_user_verification, dslogon_uuid: user.edipi) }
+
+        context 'When user has avc' do
+          let!(:user_acceptable_verified_credential) do
+            create(:user_acceptable_verified_credential, :with_avc, user_account:)
+          end
+
+          it 'sends an email' do
+            expect(VANotify::EmailJob).to receive(:perform_async).with(
+              user.email,
+              reactivation_template,
+              {}
+            )
+
+            job.perform(user)
+          end
+        end
+
+        context 'When user has ivc' do
+          let!(:user_acceptable_verified_credential) do
+            create(:user_acceptable_verified_credential, :with_ivc, user_account:)
+          end
+
+          it 'sends an email' do
+            expect(VANotify::EmailJob).to receive(:perform_async).with(
+              user.email,
+              reactivation_template,
+              {}
+            )
+
+            job.perform(user)
+          end
+        end
+
+        context 'when user does not have avc/ivc' do
+          let!(:user_acceptable_verified_credential) do
+            create(:user_acceptable_verified_credential, :without_avc_ivc, user_account:)
+          end
+
+          it 'does not send an email' do
+            expect(VANotify::EmailJob).not_to receive(:perform_async).with(
+              user.email,
+              reactivation_template,
+              {}
+            )
+
+            job.perform(user)
+          end
+        end
+      end
+
+      context 'User is mhv authenticated' do
+        let(:user) { create(:user, :mhv, authn_context: SAML::User::MHV_ORIGINAL_CSID) }
+        let(:user_verification) { create(:mhv_user_verification, mhv_uuid: user.mhv_correlation_id) }
+
+        context 'When user has avc' do
+          let!(:user_acceptable_verified_credential) do
+            create(:user_acceptable_verified_credential, :with_avc, user_account:)
+          end
+
+          it 'sends an email' do
+            expect(VANotify::EmailJob).to receive(:perform_async).with(
+              user.email,
+              reactivation_template,
+              {}
+            )
+
+            job.perform(user)
+          end
+        end
+
+        context 'When user has ivc' do
+          let!(:user_acceptable_verified_credential) do
+            create(:user_acceptable_verified_credential, :with_ivc, user_account:)
+          end
+
+          it 'sends an email' do
+            expect(VANotify::EmailJob).to receive(:perform_async).with(
+              user.email,
+              reactivation_template,
+              {}
+            )
+
+            job.perform(user)
+          end
+        end
+
+        context 'when user does not have avc/ivc' do
+          let!(:user_acceptable_verified_credential) do
+            create(:user_acceptable_verified_credential, :without_avc_ivc, user_account:)
+          end
+
+          it 'does not send an email' do
+            expect(VANotify::EmailJob).not_to receive(:perform_async).with(
+              user.email,
+              reactivation_template,
+              {}
+            )
+
+            job.perform(user)
+          end
+        end
+      end
+
+      context 'When user is login.gov authenticated' do
+        let(:user) { create(:user, :accountable_with_logingov_uuid, authn_context: IAL::LOGIN_GOV_IAL2) }
+        let(:user_verification) { create(:logingov_user_verification, logingov_uuid: user.logingov_uuid) }
+
+        context 'When user has avc' do
+          let!(:user_acceptable_verified_credential) do
+            create(:user_acceptable_verified_credential, :with_avc, user_account:)
+          end
+
+          it 'does not send an email' do
+            expect(VANotify::EmailJob).not_to receive(:perform_async).with(
+              user.email,
+              reactivation_template,
+              {}
+            )
+
+            job.perform(user)
+          end
+        end
+
+        context 'When user has ivc' do
+          let!(:user_acceptable_verified_credential) do
+            create(:user_acceptable_verified_credential, :with_ivc, user_account:)
+          end
+
+          it 'does not send an email' do
+            expect(VANotify::EmailJob).not_to receive(:perform_async).with(
+              user.email,
+              reactivation_template,
+              {}
+            )
+
+            job.perform(user)
+          end
+        end
+
+        context 'when user does not have avc/ivc' do
+          let!(:user_acceptable_verified_credential) do
+            create(:user_acceptable_verified_credential, :without_avc_ivc, user_account:)
+          end
+
+          it 'does not send an email' do
+            expect(VANotify::EmailJob).not_to receive(:perform_async).with(
+              user.email,
+              reactivation_template,
+              {}
+            )
+
+            job.perform(user)
+          end
+        end
+      end
+
+      context 'When user is idme authenticated' do
+        let(:user) { create(:user, :accountable, authn_context: LOA::IDME_LOA3_VETS) }
+        let(:user_verification) { create(:idme_user_verification, idme_uuid: user.idme_uuid) }
+
+        context 'When user has avc' do
+          let!(:user_acceptable_verified_credential) do
+            create(:user_acceptable_verified_credential, :with_avc, user_account:)
+          end
+
+          it 'does not send an email' do
+            expect(VANotify::EmailJob).not_to receive(:perform_async).with(
+              user.email,
+              reactivation_template,
+              {}
+            )
+
+            job.perform(user)
+          end
+        end
+
+        context 'When user has ivc' do
+          let!(:user_acceptable_verified_credential) do
+            create(:user_acceptable_verified_credential, :with_ivc, user_account:)
+          end
+
+          it 'does not send an email' do
+            expect(VANotify::EmailJob).not_to receive(:perform_async).with(
+              user.email,
+              reactivation_template,
+              {}
+            )
+
+            job.perform(user)
+          end
+        end
+
+        context 'when user does not have avc/ivc' do
+          let!(:user_acceptable_verified_credential) do
+            create(:user_acceptable_verified_credential, :without_avc_ivc, user_account:)
+          end
+
+          it 'does not send an email' do
+            expect(VANotify::EmailJob).not_to receive(:perform_async).with(
+              user.email,
+              reactivation_template,
+              {}
+            )
+
+            job.perform(user)
+          end
+        end
+      end
+    end
+
+    context 'When Flipper reactivation_experiment is disabled' do
+      before do
+        Flipper.disable(:reactivation_experiment)
+      end
+
+      context 'User is dslogon authenticated' do
+        let(:user) { create(:user, :dslogon) }
+        let(:user_verification) { create(:dslogon_user_verification, dslogon_uuid: user.edipi) }
+
+        context 'When user has avc' do
+          let!(:user_acceptable_verified_credential) do
+            create(:user_acceptable_verified_credential, :with_avc, user_account:)
+          end
+
+          it 'does not send an email' do
+            expect(VANotify::EmailJob).not_to receive(:perform_async).with(
+              user.email,
+              reactivation_template,
+              {}
+            )
+
+            job.perform(user)
+          end
+        end
+      end
+    end
+  end
+end
