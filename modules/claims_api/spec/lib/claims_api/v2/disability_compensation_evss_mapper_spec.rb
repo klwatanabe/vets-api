327994c09ba85d08d690653b1d18f018745ed8e7
Austin Covrig <austin.covrig@oddball.io>
2023-06-09 12:38:44 -0400

    Update spec

--- modules/claims_api/spec/lib/claims_api/v2/disability_compensation_evss_mapper_spec.rb
+++ modules/claims_api/spec/lib/claims_api/v2/disability_compensation_evss_mapper_spec.rb
@@ -79,15 +79,15 @@ describe ClaimsApi::V2::DisabilityCompensationEvssMapper do
         expect(disability[:classificationCode]).to eq('5420')
         expect(disability[:serviceRelevance]).to eq('ABCDEFG')
         expect(disability[:ratedDisabilityId]).to eq('ABCDEFGHIJKLMNOPQRSTUVWX')
-        expect(disability[:diagnosticCode]).to eq(0)
+        expect(disability[:diagnosticCode]).to eq(9999)
         expect(disability[:exposureOrEventOrInjury]).to eq('EXPOSURE')
-        expect(disability[:approximateBeginDate]).to eq({ year: 4592, month: 11, day: 4 })
+        expect(disability[:approximateBeginDate]).to eq({ year: 2018, month: 11, day: 3 })
 
-        expect(secondary[:name]).to eq('ABCDEF')
+        expect(secondary[:name]).to eq('Cancer - Musculoskeletal - Elbow')
         expect(secondary[:disabilityActionType]).to eq('SECONDARY')
         expect(secondary[:serviceRelevance]).to eq('ABCDEFGHIJKLMNOPQ')
-        expect(secondary[:classificationCode]).to eq('ABCDEFGHIJKLMNO')
-        expect(secondary[:approximateBeginDate]).to eq({ year: 9904, month: 1, day: 3 })
+        expect(secondary[:classificationCode]).to eq('249470')
+        expect(secondary[:approximateBeginDate]).to eq({ year: 2018, month: 12, day: 3 })
       end
     end
   end
