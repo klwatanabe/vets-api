32b2c4fe49d3678d4ceb68cdca83dffb5a3e3f61
Micah <micah.frazier82@gmail.com>
2023-06-10 16:38:06 -0600

    Caching all responses of BenefitsReferenceData.get_data method

--- app/controllers/v0/benefits_reference_data_controller.rb
+++ app/controllers/v0/benefits_reference_data_controller.rb
@@ -6,14 +6,28 @@ module V0
     include ActionController::Serialization
 
     def get_data
-      render json: benefits_reference_data_service
-        .get_data(path: params[:path], params: request.query_parameters).body
+
+      if params[:path] == 'disabilities'
+        render json: filtered_disabilities
+      else
+        render json: lighthouse_data
+      end
     end
 
     private
 
     def benefits_reference_data_service
       BenefitsReferenceData::Service.new
+    end
+
+    def lighthouse_data
+      Rails.cache.fetch("brd_data_#{params[:path]}",  expires_in: 30.minutes) do
+        benefits_reference_data_service
+          .get_data(path: params[:path], params: request.query_parameters).body
+      end
+    end
+    def filtered_disabilities
+      lighthouse_data.select {|k,v| v.include? params[:name]}
     end
   end
 end
