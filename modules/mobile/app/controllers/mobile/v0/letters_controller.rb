9e94a87ed9ad54265ef243c68262ea701a16d25d
Kris Pethtel <kris.pethtel@gmail.com>
2023-06-16 12:42:27 -0400

    use strong params

--- modules/mobile/app/controllers/mobile/v0/letters_controller.rb
+++ modules/mobile/app/controllers/mobile/v0/letters_controller.rb
@@ -8,6 +8,23 @@ require 'lighthouse/letters_generator/service'
 module Mobile
   module V0
     class LettersController < ApplicationController
+      # militaryService not included in all parts of the docs
+      DOWNLOAD_PARAMS = %i[
+        militaryService
+        serviceConnectedDisabilities
+        serviceConnectedEvaluation
+        nonServiceConnectedPension
+        monthlyAward
+        unemployable
+        specialMonthlyCompensation
+        adaptedHousing
+        chapter35Eligibility
+        deathResultOfDisability
+        survivorsAward
+      ].freeze
+
+
+
       before_action do
         if Flipper.enabled?(:mobile_lighthouse_letters, @current_user)
           authorize :lighthouse, :access?
@@ -15,6 +32,8 @@ module Mobile
           authorize :evss, :access?
         end
       end
+
+      after_action :increment_download_counter, only: %i[download], if: -> { response.successful? }
 
       # returns list of letters available for a given user. List includes letter display name and letter type
       def index
@@ -40,7 +59,6 @@ module Mobile
       def download
         if params[:format] == 'json'
           letter = lighthouse_service.get_letter(icn, params[:type], download_options_hash)
-          increment_download_counter
           return render json: Mobile::V0::LetterSerializer.new(current_user.uuid, letter)
         end
 
@@ -55,7 +73,6 @@ module Mobile
 
                      download_service.download_letter(params[:type], download_options)
                    end
-        increment_download_counter
         send_data response,
                   filename: "#{params[:type]}.pdf",
                   type: 'application/pdf',
@@ -82,9 +99,10 @@ module Mobile
       end
 
       def download_options_hash
-        return {} if download_options.blank?
-
-        download_options.split('&').to_h { |pair| pair.split('=') }.except('format')
+        permitted_params = params.permit(DOWNLOAD_PARAMS)
+        permitted_params.to_h
+                        .transform_values { |v| ActiveModel::Type::Boolean.new.cast(v) }
+                        .transform_keys { |k| k.camelize(:lower) }
       end
 
       def letter_info_adapter
