644f7892305575dc045839cf0490fd6ffb1c5ac3
Joseph Hall <joe.hall@thoughtworks.com>
2023-07-05 16:53:56 -0400

    adds two tests for two static data methods, auth'd and unauth'd

--- app/controllers/v0/ask_va/static_data_auth_controller.rb
+++ app/controllers/v0/ask_va/static_data_auth_controller.rb
@@ -3,25 +3,24 @@
 require 'date'
 
 module V0
-  module AskVa
-    class AskVaStaticDataController < ApplicationController
+  module AskVA
+    class StaticDataAuthController < ApplicationController
       def index
         data = {
+            'Ruchi': { 'data-info': 'ruchi.shah@thoughtworks.com' },
             'Eddie': { 'data-info': 'eddie.otero@oddball.io' },
             'Jacob': { 'data-info': 'jacob@docme360.com' },
             'Joe': { 'data-info': 'joe.hall@thoughtworks.com' },
             'Khoa': { 'data-info': 'khoa.nguyen@oddball.io' },
         }
-        render json: {
-          data:
-        }
+        render json: data, status: :ok
       rescue => e
         service_exception_handler(e)
       end
 
       private
       def service_exception_handler(exception)
-        context = 'An error occurred while attempting to retrieve the list of devs.'
+        context = 'An error occurred while attempting to retrieve the authenticated list of devs.'
         log_exception_to_sentry(exception, 'context' => context)
         render nothing: true, status: :internal_server_error
       end
