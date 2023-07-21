71733eb9baafbe9028128e28757fb7ed0c9e4bc0
Eric Tillberg <Thrillberg@users.noreply.github.com>
2023-07-21 10:25:09 -0400

    Refactor getting attachments

--- modules/simple_forms_api/app/models/simple_forms_api/vba_21_4142.rb
+++ modules/simple_forms_api/app/models/simple_forms_api/vba_21_4142.rb
@@ -21,5 +21,28 @@ module SimpleFormsApi
         'businessLine' => 'CMP'
       }
     end
+
+    def self.attachments(data)
+      text = <<~STRING
+        RELATIONSHIP TO VETERAN/CLAIMANT:  \
+        #{data.dig('preparer_identification', 'relationship_to_veteran') ||
+          data.dig('preparer_identification', 'other')} \
+        #{data.dig('preparer_identification', 'preparer_full_name', 'first')} \
+        #{data.dig('preparer_identification', 'preparer_full_name', 'middle')} \
+        #{data.dig('preparer_identification', 'preparer_full_name', 'last')} \
+        #{data.dig('preparer_identification', 'preparer_title')} \
+        #{data.dig('preparer_identification', 'preparer_organization')} \
+        #{data.dig('preparer_identification', 'preparer_address', 'street')} \
+        #{data.dig('preparer_identification', 'preparer_address', 'street2')} \
+        #{data.dig('preparer_identification', 'preparer_address', 'city')} \
+        #{data.dig('preparer_identification', 'preparer_address', 'state')} \
+        #{data.dig('preparer_identification', 'preparer_address', 'postal_code')} \
+        #{data.dig('preparer_identification', 'court_appointment_info')}
+      STRING
+      pdf = Prawn::Document.new
+      pdf.text text
+      pdf.render_file("tmp/vba_21_4142-attachment-tmp.pdf")
+      ["tmp/vba_21_4142-attachment-tmp.pdf"]
+    end
   end
 end
