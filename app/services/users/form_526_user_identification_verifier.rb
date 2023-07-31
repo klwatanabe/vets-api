c6e4741d7a3f1714b3292fea3fe41c41947bffeb
Nathan Burgess <burgess.nathan28@gmail.com>
2023-07-31 12:44:45 -0400

    PR feedback: refactor verifier to use service object pattern
    Refactors Form526UserIdentificationVerifier to allow for use of the classic service object pattern, with the service being initialized as a disposable singleton, intialized by its class and discarded.

--- app/services/users/form_526_user_identification_verifier.rb
+++ app/services/users/form_526_user_identification_verifier.rb
@@ -4,9 +4,19 @@ module Users
   class Form526UserIdentificationVerifier
     FORM526_REQUIRED_IDENTIFIERS = %w[participant_id birls_id ssn birth_date edipi].freeze
 
+    def self.call(*args)
+      new(*args).call
+    end
+
     def initialize(user)
       @user = user
     end
+
+    def call
+      missing_identifiers
+    end
+
+    private
 
     def missing_identifiers
       FORM526_REQUIRED_IDENTIFIERS.select { |id| @user[id].blank? }
--- spec/services/users/form526_user_identification_verifier_spec.rb
+++ spec/services/users/form526_user_identification_verifier_spec.rb
@@ -5,20 +5,19 @@ require 'rails_helper'
 RSpec.describe Users::Form526UserIdentificationVerifier do
   describe '#missing_identifiers' do
     let(:user) { build(:user) }
-    let(:verifier) { Users::Form526UserIdentificationVerifier.new(user) }
 
     describe 'participant_id validation' do
       context 'when participant_id is missing' do
         it "returns an array with 'participant_id' in it" do
           allow(user).to receive(:participant_id).and_return(nil)
-          expect(verifier.missing_identifiers).to include('participant_id')
+          expect(Users::Form526UserIdentificationVerifier.call(user)).to include('participant_id')
         end
       end
 
       context 'when participant_id is present' do
         it 'does not return an array with participant_id in it' do
           allow(user).to receive(:participant_id).and_return('8675309')
-          expect(verifier.missing_identifiers).not_to include('participant_id')
+          expect(Users::Form526UserIdentificationVerifier.call(user)).not_to include('participant_id')
         end
       end
     end
@@ -27,14 +26,14 @@ RSpec.describe Users::Form526UserIdentificationVerifier do
       context 'when birls_id is missing' do
         it "returns an array with 'birls_id' in it" do
           allow(user).to receive(:birls_id).and_return(nil)
-          expect(verifier.missing_identifiers).to include('birls_id')
+          expect(Users::Form526UserIdentificationVerifier.call(user)).to include('birls_id')
         end
       end
 
       context 'when birls_id is present' do
         it 'does not return an array with birls_id in it' do
           allow(user).to receive(:birls_id).and_return('8675309')
-          expect(verifier.missing_identifiers).not_to include('birls_id')
+          expect(Users::Form526UserIdentificationVerifier.call(user)).not_to include('birls_id')
         end
       end
     end
@@ -43,14 +42,14 @@ RSpec.describe Users::Form526UserIdentificationVerifier do
       context 'when ssn is missing' do
         it "returns an array with 'ssn' in it" do
           allow(user).to receive(:ssn).and_return(nil)
-          expect(verifier.missing_identifiers).to include('ssn')
+          expect(Users::Form526UserIdentificationVerifier.call(user)).to include('ssn')
         end
       end
 
       context 'when ssn is present' do
         it 'does not return an array with ssn in it' do
           allow(user).to receive(:ssn).and_return('8675309')
-          expect(verifier.missing_identifiers).not_to include('ssn')
+          expect(Users::Form526UserIdentificationVerifier.call(user)).not_to include('ssn')
         end
       end
     end
@@ -59,14 +58,14 @@ RSpec.describe Users::Form526UserIdentificationVerifier do
       context 'when birth_date is missing' do
         it "returns an array with 'birth_date' in it" do
           allow(user).to receive(:birth_date).and_return(nil)
-          expect(verifier.missing_identifiers).to include('birth_date')
+          expect(Users::Form526UserIdentificationVerifier.call(user)).to include('birth_date')
         end
       end
 
       context 'when birth_date is present' do
         it 'does not return an array with birth_date in it' do
           allow(user).to receive(:birth_date).and_return('1985-10-26')
-          expect(verifier.missing_identifiers).not_to include('birth_date')
+          expect(Users::Form526UserIdentificationVerifier.call(user)).not_to include('birth_date')
         end
       end
     end
@@ -75,14 +74,14 @@ RSpec.describe Users::Form526UserIdentificationVerifier do
       context 'when edipi is missing' do
         it "returns an array with 'edipi' in it" do
           allow(user).to receive(:edipi).and_return(nil)
-          expect(verifier.missing_identifiers).to include('edipi')
+          expect(Users::Form526UserIdentificationVerifier.call(user)).to include('edipi')
         end
       end
 
       context 'when edipi is present' do
         it 'does not return an array with edipi in it' do
           allow(user).to receive(:edipi).and_return('8675309')
-          expect(verifier.missing_identifiers).not_to include('edipi')
+          expect(Users::Form526UserIdentificationVerifier.call(user)).not_to include('edipi')
         end
       end
     end
