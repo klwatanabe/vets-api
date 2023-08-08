eb455516e2b8d1b92f35b0456ced853db466ce03
Ryan McNeil <ryan.mcneil@oddball.io>
2023-08-08 16:17:33 -0600

    finish specs

--- lib/flipper/admin_user_constraint.rb
+++ lib/flipper/admin_user_constraint.rb
@@ -3,16 +3,23 @@
 module Flipper
   class AdminUserConstraint
     def self.matches?(request)
-      # puts 'request_path: ' + request.method + ' ' + request.path
+      # Raise exception for any unauthorized toggles
+      if request.method == 'POST' && request.path.include?('/boolean')
+        return true if is_authorized?(request.session[:flipper_user])
 
+        raise Common::Exceptions::Forbidden
+      end
+
+      # If Authenticated through GitHub, check authorization to determine what can be shown in views
       if request.session[:flipper_user].present?
         user = request.session[:flipper_user]
         RequestStore.store[:flipper_user_email_for_log] = user&.email
-        authorize(user)
+        RequestStore.store[:flipper_authorized] = is_authorized?(user)
 
         return true
       end
 
+      # allow GET requests (minus the callback, which needs to pass through to finish auth flow)
       # return true if (request.method == 'GET' && request.path.exclude?('/callback')) || Rails.env.development?
       return true if request.method == 'GET' && request.path.exclude?('/callback')
 
@@ -25,11 +32,11 @@ module Flipper
       warden.authenticate!(scope: :flipper)
     end
 
-    def self.authorize(user)
+    def self.is_authorized?(user)
       org_name = Settings.flipper.github_organization
       team_id = Settings.flipper.github_team
 
-      RequestStore.store[:flipper_authorized] = user.organization_member?(org_name) && user.team_member?(team_id)
+      user&.organization_member?(org_name) && user&.team_member?(team_id)
     end
   end
 end
--- spec/requests/flipper_request_spec.rb
+++ spec/requests/flipper_request_spec.rb
@@ -1,18 +1,19 @@
 # frozen_string_literal: true
 
 require 'rails_helper'
+require 'nokogiri'
 
 RSpec.describe 'Flipper UI' do
-  def bypass_flipper_authenticity_token
-    Rails.application.routes.draw do
-      mount Flipper::UI.app(
-        Flipper.instance,
-        rack_protection: { except: :authenticity_token }
-      ) => '/flipper', constraints: Flipper::AdminUserConstraint
+    def bypass_flipper_authenticity_token
+      Rails.application.routes.draw do
+        mount Flipper::UI.app(
+          Flipper.instance,
+          rack_protection: { except: :authenticity_token }
+        ) => '/flipper', constraints: Flipper::AdminUserConstraint
+      end
+      yield
+      Rails.application.reload_routes!
     end
-    yield
-    Rails.application.reload_routes!
-  end
 
   include Warden::Test::Helpers
 
@@ -26,142 +27,264 @@ RSpec.describe 'Flipper UI' do
   end
   let(:user) { Warden::GitHub::User.new(default_attrs) }
 
+  github_oauth_message = 'User must authenticate with GitHub to edit feature toggles'
+
   before do
     allow_any_instance_of(Warden::Proxy).to receive(:authenticate!).and_return(user)
     allow_any_instance_of(Warden::Proxy).to receive(:user).and_return(user)
-    # allow_any_instance_of(ActionDispatch::Request).to receive(:session) { { "flipper_user": user } }
+    allow(user).to receive(:organization_member?).with(Settings.sidekiq.github_organization).and_return(false)
+    allow(user).to receive(:team_member?).with(Settings.sidekiq.github_team).and_return(false)
   end
 
-  context 'Authorized user (organization and team membership)' do
-    before do
-      # allow_any_instance_of(Warden::Proxy).to receive(:authenticate!).and_return(user)
-      # allow_any_instance_of(Warden::Proxy).to receive(:user).and_return(user)
-      # allow_any_instance_of(ActionDispatch::Request).to receive(:session) { { "flipper_user": user } }
-      # sign_in_as(user)
-      allow(user).to receive(:organization_member?).with(Settings.sidekiq.github_organization).and_return(true)
-      allow(user).to receive(:team_member?).with(Settings.sidekiq.github_team).and_return(true)
+  context 'GET /flipper/features' do
+    context 'Unauthenticated user' do
+      it 'is told to sign in with GitHub to access features' do
+        get '/flipper/features'
+        expect(response.body).to include(github_oauth_message)
+        assert_response :success
+      end
 
-      Flipper.enable(:test_feature)
+      it 'is shown a button to sign in with GitHub' do
+        get '/flipper/features'
+        body = Nokogiri::HTML(response.body)
+        signin_button = body.at_css('button:contains("Sign in to GitHub")')
+        expect(signin_button).to_not be_nil
+        assert_response :success
+      end
+
+      it 'can see a list of features, but they are NOT clickable (NOT hrefs to feature page)' do
+        get '/flipper/features'
+        body = Nokogiri::HTML(response.body)
+        feature_link = body.at_css('a[href*="/flipper/features/this_is_only_a_test"]')
+        expect(response.body).to include('this_is_only_a_test')
+        expect(feature_link).to be_nil
+        assert_response :success
+      end
     end
 
-    # let(:user) { build(:user, :loa3) }
+    context 'Authenticated user (through GitHub Oauth)' do
+      before do
+        # Mimic the functionality of the end of the OAuth handshake, where #finalize_flow! (`warden_github.rb`)
+        # is called, setting the value of request.session[:flipper_user] to the mocked Warden::Github user
+        allow_any_instance_of(ActionDispatch::Request).to receive(:session) { { flipper_user: user } }
+      end
 
-    it 'can display list of features' do
-      get '/flipper/features', params: nil
-      # expect(response.body).to include('flipper/features') # why test this?
-      assert_response :success
-    end
+      it 'is not shown a notice to sign into GitHub' do
+        get '/flipper/features'
+        expect(response.body).not_to include(github_oauth_message)
+        assert_response :success
+      end
 
-    it 'can toggle features' do
-      expect(Flipper.enabled?(:test_feature)).to be true
-      bypass_flipper_authenticity_token do
-        post '/flipper/features/test_feature/boolean', params: nil
-        assert_response :found
-        expect(Flipper.enabled?(:test_feature)).to be false
+      it 'is not shown a button to sign in with GitHub' do
+        get '/flipper/features'
+        body = Nokogiri::HTML(response.body)
+        signin_button = body.at_css('button:contains("Sign in to GitHub")')
+        expect(signin_button).to be_nil
+        assert_response :success
+      end
+
+      context 'Authorized user (organization and team membership)' do
+        before do
+          allow(user).to receive(:organization_member?).with(Settings.sidekiq.github_organization).and_return(true)
+          allow(user).to receive(:team_member?).with(Settings.sidekiq.github_team).and_return(true)
+        end
+
+        it 'can see a list of features and they are clickable (hrefs to feature page)' do
+          get '/flipper/features'
+          body = Nokogiri::HTML(response.body)
+          feature_link = body.at_css('a[href*="/flipper/features/this_is_only_a_test"]')
+          expect(feature_link).to_not be_nil
+          assert_response :success
+        end
+      end
+
+      context 'Unauthorized user' do
+        unauthorized_message = 'You are not authorized to perform any actions'
+
+        it 'can see a list of features, but they are NOT clickable (NOT hrefs to feature page)' do
+          get '/flipper/features'
+          body = Nokogiri::HTML(response.body)
+          feature_link = body.at_css('a[href*="/flipper/features/this_is_only_a_test"]')
+          expect(response.body).to include('this_is_only_a_test')
+          expect(feature_link).to be_nil
+          assert_response :success
+        end
+
+        context 'without organization membership' do
+          it 'is told that they are unauthorized and links to documentation' do
+            allow(user).to receive(:organization_member?).with(Settings.sidekiq.github_organization).and_return(false)
+
+            get '/flipper/features'
+            body = Nokogiri::HTML(response.body)
+            docs_link = body.at_css('a[href*="depo-platform-documentation"]')
+            expect(response.body).to include(unauthorized_message)
+            expect(docs_link).to_not be_nil
+          end
+        end
+
+        context 'without team membership' do
+          it 'is told that they are unauthorized and links to documentation' do
+            allow(user).to receive(:organization_member?).with(Settings.sidekiq.github_organization).and_return(true)
+            allow(user).to receive(:team_member?).with(Settings.sidekiq.github_team).and_return(false)
+
+            get '/flipper/features'
+            body = Nokogiri::HTML(response.body)
+            docs_link = body.at_css('a[href*="depo-platform-documentation"]')
+            expect(response.body).to include(unauthorized_message)
+            expect(docs_link).to_not be_nil
+            end
+        end
       end
     end
   end
 
-  context 'Unauthorized user' do
-    it 'feature route is read only' do
-      get '/flipper/features', params: nil
-      assert_response :success
-      expect(response.body).not_to include('flipper/features')
-    end
+  context 'GET flipper/features/:some_feature' do
+    context 'Unauthenticated user' do
+      it 'is told to sign in with GitHub to access features' do
+        get '/flipper/features/this_is_only_a_test'
+        expect(response.body).to include(github_oauth_message)
+        assert_response :success
+      end
 
-    context 'without organization membership' do
-      it 'cannot toggle features and returns 404' do
-        # require 'pry'; binding.pry;
-        Flipper.enable(:test_feature)
-        allow(user).to receive(:organization_member?).with(Settings.flipper.github_organization).and_return(false)
+      it 'is shown a button to sign in with GitHub' do
+        get '/flipper/features/this_is_only_a_test'
+        body = Nokogiri::HTML(response.body)
+        signin_button = body.at_css('button:contains("Sign in to GitHub")')
+        expect(signin_button).to_not be_nil
+        assert_response :success
+      end
 
-        # bypass_flipper_authenticity_token do
-          expect do
-            post '/flipper/features/test_feature/boolean', params: nil
-          end.to raise_error(Common::Exceptions::Forbidden)
-        # end
+      it 'cannot see the feature name in the title (h1) or button to enable/disable' do
+        get '/flipper/features/this_is_only_a_test'
+        body = Nokogiri::HTML(response.body)
+        title = body.at_css('h1:contains("this_is_only_a_test")')
+        toggle_button = body.at_css('button:contains("for everyone")')
+        expect(title).to be_nil
+        expect(toggle_button).to be_nil
+        assert_response :success
       end
     end
 
-    context 'without team membership' do
-      it 'cannot toggle features and returns 404' do
-        Flipper.enable(:test_feature)
-        allow(user).to receive(:organization_member?).with(Settings.flipper.github_organization).and_return(true)
-        allow(user).to receive(:team_member?).with(Settings.flipper.github_team).and_return(false)
+    context 'Authenticated user (through GitHub Oauth)' do
+      before do
+        # Mimic the functionality of the end of the OAuth handshake, where #finalize_flow! (`warden_github.rb`)
+        # is called, setting the value of request.session[:flipper_user] to the mocked Warden::Github user
+        allow_any_instance_of(ActionDispatch::Request).to receive(:session) { { flipper_user: user } }
+      end
 
-        # bypass_flipper_authenticity_token do
+      it 'is not shown a notice to sign into GitHub' do
+        get '/flipper/features/this_is_only_a_test'
+        expect(response.body).not_to include(github_oauth_message)
+        assert_response :success
+      end
+
+      it 'is not shown a button to sign in with GitHub' do
+        get '/flipper/features/this_is_only_a_test'
+        body = Nokogiri::HTML(response.body)
+        signin_button = body.at_css('button:contains("Sign in to Github")')
+        expect(signin_button).to be_nil
+        assert_response :success
+      end
+
+      context 'Authorized user (organization and team membership)' do
+        before do
+          allow(user).to receive(:organization_member?).with(Settings.sidekiq.github_organization).and_return(true)
+          allow(user).to receive(:team_member?).with(Settings.sidekiq.github_team).and_return(true)
+          Flipper.disable(:this_is_only_a_test)
+        end
+
+        it 'can see the feature name in title and button to enable/disable feature' do
+          get '/flipper/features/this_is_only_a_test'
+          body = Nokogiri::HTML(response.body)
+          title = body.at_css('h1:contains("this_is_only_a_test")')
+          toggle_button = body.at_css('button:contains("Enable for everyone")')
+          expect(title).to_not be_nil
+          expect(toggle_button).to_not be_nil
+          assert_response :success
+        end
+      end
+      
+      context 'Unauthorized user' do
+        unauthorized_message = 'You are not authorized to perform any actions'
+
+        it 'cannot see the feature name in the title (h1) or button to enable/disable' do
+          get '/flipper/features/this_is_only_a_test'
+          body = Nokogiri::HTML(response.body)
+          title = body.at_css('h1:contains("this_is_only_a_test")')
+          toggle_button = body.at_css('button:contains("for everyone")')
+          expect(title).to be_nil
+          expect(toggle_button).to be_nil
+          assert_response :success
+        end
+
+        context 'without organization membership' do
+          it 'is told that they are unauthorized and links to documentation' do
+            allow(user).to receive(:organization_member?).with(Settings.sidekiq.github_organization).and_return(false)
+
+            get '/flipper/features/this_is_only_a_test'
+            body = Nokogiri::HTML(response.body)
+            docs_link = body.at_css('a[href*="depo-platform-documentation"]')
+            expect(response.body).to include(unauthorized_message)
+            expect(docs_link).to_not be_nil
+          end
+        end
+
+        context 'without team membership' do
+          it 'is told that they are unauthorized and links to documentation' do
+            allow(user).to receive(:organization_member?).with(Settings.sidekiq.github_organization).and_return(true)
+            allow(user).to receive(:team_member?).with(Settings.sidekiq.github_team).and_return(false)
+
+            get '/flipper/features/this_is_only_a_test'
+            body = Nokogiri::HTML(response.body)
+            docs_link = body.at_css('a[href*="depo-platform-documentation"]')
+            expect(response.body).to include(unauthorized_message)
+            expect(docs_link).to_not be_nil
+            end
+        end
+      end
+    end
+  end
+
+  context 'POST flipper/features/:some_feature' do
+    context 'Unauthenticated User' do
+      it 'cannot toggle features and returns 403' do
+        bypass_flipper_authenticity_token do
           expect do
-            post '/flipper/features/test_feature/boolean', params: nil
+            post '/flipper/features/this_is_only_a_test/boolean'
           end.to raise_error(Common::Exceptions::Forbidden)
-        # end
+        end
+      end
+    end
+
+    context 'Authenticated User' do
+      before do
+        allow_any_instance_of(ActionDispatch::Request).to receive(:session) { { flipper_user: user } }
+      end
+
+      context 'Unauthorized User' do
+        it 'cannot toggle features and returns 403' do
+          bypass_flipper_authenticity_token do
+            expect do
+              post '/flipper/features/this_is_only_a_test/boolean'
+            end.to raise_error(Common::Exceptions::Forbidden)
+          end
+        end
+      end
+
+      context 'Authorized User' do
+        it 'can toggle features' do
+          allow(user).to receive(:organization_member?).with(Settings.flipper.github_organization).and_return(true)
+          allow(user).to receive(:team_member?).with(Settings.flipper.github_team).and_return(true)
+          Flipper.enable(:this_is_only_a_test)
+  
+          bypass_flipper_authenticity_token do
+            expect(Flipper.enabled?(:this_is_only_a_test)).to be true
+            post '/flipper/features/this_is_only_a_test/boolean', params: nil
+            assert_response :found
+            expect(Flipper.enabled?(:this_is_only_a_test)).to be false
+          end
+        end
       end
     end
   end
 end
-
-# RSpec.describe 'Flipper UI' do
-#   def bypass_flipper_authenticity_token
-#     Rails.application.routes.draw do
-#       mount Flipper::UI.app(
-#         Flipper.instance,
-#         rack_protection: { except: :authenticity_token }
-#       ) => '/flipper', constraints: Flipper::AdminUserConstraint.new
-#     end
-#     yield
-#     Rails.application.reload_routes!
-#   end
-
-#   context 'with authenticated admin user' do
-#     before do
-#       sign_in_as(user)
-#       allow(Settings.flipper).to receive(:admin_user_emails).and_return(user.email)
-#       Flipper.enable(:test_feature)
-#     end
-
-#     context 'with LOA1 access' do
-#       let(:user) { build(:user) }
-
-#       it 'does not allow feature toggling' do
-#         post '/flipper/features/test_feature/boolean', params: nil
-#         assert_response :not_found
-#         expect(JSON.parse(response.body)['errors'][0]['detail']).to include('There are no routes matching your request')
-#       end
-#     end
-
-#     context 'with LOA3 access' do
-#       let(:user) { build(:user, :loa3) }
-
-#       it 'Displays list of features' do
-#         get '/flipper/features', params: nil
-#         expect(response.body).to include('flipper/features')
-#         assert_response :success
-#       end
-
-#       it 'Allows user to toggle feature' do
-#         bypass_flipper_authenticity_token do
-#           expect(Flipper.enabled?(:test_feature)).to be true
-#           post '/flipper/features/test_feature/boolean', params: nil
-#           assert_response :found
-#           expect(Flipper.enabled?(:test_feature)).to be false
-#         end
-#       end
-#     end
-#   end
-
-#   context 'when unauthenticated' do
-#     it 'feature route is read only' do
-#       get '/flipper/features', params: nil
-#       assert_response :success
-#       expect(response.body).not_to include('flipper/features')
-#     end
-
-#     it 'does not allow feature toggles' do
-#       bypass_flipper_authenticity_token do
-#         Flipper.enable(:test_feature)
-#         expect do
-#           post '/flipper/features/test_feature/boolean', params: nil
-#         end.to raise_error(ActionController::RoutingError)
-#       end
-#     end
-#   end
-# end
