644251067628130c969a11539822ba9544a21923
Evan Smith <evan.smith@oddball.io>
2023-08-10 10:24:25 -0400

    first pass at transitioning 686c and 674 jobs away from a user record

--- app/workers/bgs/submit_form686c_job.rb
+++ app/workers/bgs/submit_form686c_job.rb
@@ -11,29 +11,30 @@ module BGS
 
     sidekiq_options retry: false
 
-    def perform(user_uuid, saved_claim_id, vet_info)
-      user = User.find(user_uuid)
-      Rails.logger.info('BGS::SubmitForm686cJob running!', { user_uuid:, saved_claim_id:, icn: user&.icn })
-      in_progress_form = InProgressForm.find_by(form_id: FORM_ID, user_uuid:)
+    def perform(user_uuid, icn, saved_claim_id, vet_info, va_profile_email, email, first_name, ssn, participant_id, common_name)
+      #user = User.find(user_uuid)
+      user_account = UserAccount.find_by(icn:)
+      Rails.logger.info('BGS::SubmitForm686cJob running!', { saved_claim_id:, icn:  })
+      in_progress_form = InProgressForm.find_by(form_id: FORM_ID, user_account_id: user_account&.id)
       in_progress_copy = in_progress_form_copy(in_progress_form)
       claim_data = valid_claim_data(saved_claim_id, vet_info)
       normalize_names_and_addresses!(claim_data)
-
-      BGS::Form686c.new(user).submit(claim_data)
+      temp_user = generate_temp_user(first_name, ssn, email, va_profile_email, participant_id, icn, user_uuid, common_name)
+      BGS::Form686c.new(temp_user).submit(claim_data)
 
       # If Form 686c job succeeds, then enqueue 674 job.
       claim = SavedClaim::DependencyClaim.find(saved_claim_id)
-      BGS::SubmitForm674Job.perform_async(user_uuid, saved_claim_id, vet_info) if claim.submittable_674?
+      BGS::SubmitForm674Job.perform_async(user_uuid, icn, saved_claim_id, vet_info, va_profile_email, email, first_name, ssn, participant_id, common_name) if claim.submittable_674?
 
-      send_confirmation_email(user)
+      send_confirmation_email(user_uuid, va_profile_email, first_name) 
 
       in_progress_form&.destroy
-      Rails.logger.info('BGS::SubmitForm686cJob succeeded!', { user_uuid:, saved_claim_id:, icn: user&.icn })
+      Rails.logger.info('BGS::SubmitForm686cJob succeeded!', { user_uuid:, saved_claim_id:, icn:})
     rescue => e
-      Rails.logger.error('BGS::SubmitForm686cJob failed!', { user_uuid:, saved_claim_id:, icn: user&.icn, error: e.message }) # rubocop:disable Layout/LineLength
+      Rails.logger.error('BGS::SubmitForm686cJob failed!', { user_uuid:, saved_claim_id:, icn:, error: e.message }) # rubocop:disable Layout/LineLength
       log_message_to_sentry(e, :error, {}, { team: 'vfs-ebenefits' })
       salvage_save_in_progress_form(FORM_ID, user_uuid, in_progress_copy)
-      DependentsApplicationFailureMailer.build(user).deliver_now if user.present?
+      DependentsApplicationFailureMailer.build(temp_user).deliver_now if temp_user.present?
     end
 
     private
@@ -48,14 +49,27 @@ module BGS
       claim.formatted_686_data(vet_info)
     end
 
-    def send_confirmation_email(user)
-      return if user.va_profile_email.blank?
+    def send_confirmation_email(user_uuid, va_profile_email, first_name)
+      return if va_profile_email.blank?
 
       VANotify::ConfirmationEmail.send(
-        email_address: user.va_profile_email,
+        email_address: va_profile_email,
         template_id: Settings.vanotify.services.va_gov.template_id.form686c_confirmation_email,
-        first_name: user&.first_name&.upcase,
-        user_uuid_and_form_id: "#{user.uuid}_#{FORM_ID}"
+        first_name: first_name&.upcase,
+        user_uuid_and_form_id: "#{user_uuid}_#{FORM_ID}"
+      )
+    end
+
+    def generate_temp_user(first_name, ssn, email, va_profile_email, participant_id, icn, uuid, common_name)
+      OpenStruct.new(
+        first_name: first_name, 
+        ssn: ssn,
+        email: email,
+        va_profile_email: va_profile_email,
+        participant_id: participant_id,
+        icn: icn,
+        uuid: uuid, 
+        common_name: common_name
       )
     end
   end
