96f57f91c54d042a73cf20903fb006064265f071
Lindsey Hattamer <Lindsey.Hattamer@oddball.io>
2023-08-15 12:31:23 -0400

    feat: add check for new/updated files

---
+++ .github/scripts/check_codeowners.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -e
+
+# All added or modified files in this PR
+CHANGED_FILES=$(git diff --name-only --diff-filter=AM HEAD~1 HEAD)
+
+check_in_codeowners() {
+    local file="$1"
+    while [[ "$file" != '.' && "$file" != '/' ]]; do
+        # Check if the file or directory is in CODEOWNERS
+        if grep -qE "^\s*${file}(\s+|\$)" .github/CODEOWNERS; then
+            return 0
+        fi
+        # Move to the parent directory
+        file=$(dirname "$file")
+    done
+    return 1
+}
+for FILE in $CHANGED_FILES
+do
+  # Check if the file or any of its parent directories are in CODEOWNERS
+  if ! check_in_codeowners "$FILE"; then
+    echo "Error: $FILE (or its parent directories) does not have a CODEOWNERS entry."
+    exit 1
+  fi
+done
+
+echo "All changed files or their parent directories have a CODEOWNERS entry."
