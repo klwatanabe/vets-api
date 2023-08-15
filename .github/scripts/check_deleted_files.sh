ca4d0c2f6471a2ade4fe837779054c8f612f009f
Lindsey Hattamer <Lindsey.Hattamer@oddball.io>
2023-08-15 16:04:12 -0400

    don't use set-output. deprecated

--- .github/scripts/check_deleted_files.sh
+++ .github/scripts/check_deleted_files.sh
@@ -30,7 +30,7 @@ do
     else
       # The entire directory has been deleted, but its reference still exists in CODEOWNERS
       echo "Error: $FILE (or its parent directories) is deleted but its reference still exists in CODEOWNERS."
-      echo "::set-output name=offending_file::$FILE"
+      echo "offending_file=$FILE" >> $GITHUB_ENV
       exit 1
     fi
   fi
