4146eba923b81a6405d8062433035c3970d39438
Luke Short <lukeshortemail@gmail.com>
2023-08-30 11:06:31 -0700

    update spec to follow client.rb renaming

--- spec/lib/virtual_regional_office/integration/client_spec.rb
+++ spec/lib/virtual_regional_office/integration/client_spec.rb
@@ -5,10 +5,10 @@ require 'virtual_regional_office/client'
 
 # rubocop:disable RSpec/FilePath
 RSpec.describe VirtualRegionalOffice::Client, :vcr do
-  describe '#classify_contention_by_diagnostic_code' do
+  describe '#classify_single_contention' do
     context 'with a contention classification request' do
       subject(:client) do
-        described_class.new.classify_contention_by_diagnostic_code(
+        described_class.new.classify_single_contention(
           diagnostic_code: 5235,
           claim_id: 190,
           form526_submission_id: 179
